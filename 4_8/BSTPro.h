c23ece1993512e1db95b3f9fb796ea19efb3fafd
Hazi <hazi@hazi-Lenovo-S10-3c.(none)>
2013-05-21 23:47:59 +0800

    add 4_8 temp(to better use const)

---
+++ 4_8/BSTPro.h
@@ -0,0 +1,46 @@
+#include <list>
+#include "BST.h"
+using namespace std;
+
+template <class K,class V>
+class BSTPro:public BST<K,V>
+{
+	private:
+		typedef typename BST<K,V>::node node;
+		void printPath(list<const node*> &path,typename list<const node*>::reverse_iterator it)
+		{
+			cout<<(*it)->key<<'\t';
+			while(it!=path.rbegin())
+			{
+				it--;
+				cout<<(*it)->key<<'\t';
+			}
+			cout<<endl;
+		}
+	       void	findSumPath(const node* end,const int sum,list<const node*> path)
+	       {
+		       if(NULL == end)
+		       {
+			       return;
+		       }
+		       int temp = sum;
+		       path.push_back(end);
+		       for(typename list<const node*>::reverse_iterator it=path.rbegin();it!=path.rend();it++)
+		       {
+			       temp -= (*it)->value;
+			       if(0 == temp)
+			       {
+				       this->printPath(path,it);
+			       }
+		       }
+		       findSumPath(end->leftChild,sum,path);
+		       findSumPath(end->rightChild,sum,path);
+	       }
+	public:
+	       void findSumPath(const int sum)
+	       {
+		       list<const node*> ls;
+		       findSumPath(this->root,sum,ls);
+	       }
+
+};
