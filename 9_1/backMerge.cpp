4e0e93546ca6e363d8533affd2cdc15192f38b94
truehazi@gmail.com <truehazi@gmail.com>
2013-07-18 18:19:23 +0800

    add 9_1

---
+++ 9_1/backMerge.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+using namespace std;
+
+void backMerge(char a[],char b[],int al,int bl)
+{
+	unsigned int cur = al + bl;
+	al--;
+	bl--;
+	a[cur] = '\0';
+	while(al>=0 && bl>=0)
+	{
+		a[--cur] = a[al]<b[bl]?b[bl--]:a[al--];
+	}
+
+	while(bl>=0)
+	{
+		a[--cur] = b[bl--];
+	}
+}
+
+int main(int argc,char* argv[])
+{
+	char a[256] = {0};
+	char b[256] = {0};
+	printf("Please enter two sorted strings:\r\n");
+	scanf("%s%s",a,b);
+	backMerge(a,b,strlen(a),strlen(b));
+	printf("sorted string %s\n",a);
+	return 0;
+}
